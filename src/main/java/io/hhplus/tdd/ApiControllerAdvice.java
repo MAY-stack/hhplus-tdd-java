c7e5f4c26990e4492a6c852d38860cf42c89e0a7
Marisol <s.min.g7904@gmail.com>
2024-12-20 09:55:05 +0900

    UserNotFoundException,IllegalArgumentException Handler 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -4,11 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import io.hhplus.tdd.excpetion.UserNotFoundException;
+
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+    }
+
+    @ExceptionHandler(value = UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseEntity.status(404).body(new ErrorResponse("404", e.getMessage()));
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(404).body(new ErrorResponse("400", e.getMessage()));
     }
 }
